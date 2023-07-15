9fec235082c9f25227fddae37a863ac7a5e5c5fb
LiwZhong    (¬_¬") <74370274+VanTrung0307@users.noreply.github.com>
2023-07-15 20:22:31 +0700

    change home icon

--- src/components/ConfirmResetPassword.jsx
+++ src/components/ConfirmResetPassword.jsx
@@ -64,8 +64,28 @@ const ConfirmResetPassword = () => {
     }
   };
 
+  const handleExitToMainPage = () => {
+    window.location.href = "/main";
+  };
+
   return (
     <section className="grid h-screen place-content-center bg-slate-900 text-slate-300">
+      <button
+        className="absolute top-4 right-4 bg-black text-white rounded-full p-2 hover:bg-blue-500 transition duration-200"
+        onClick={handleExitToMainPage}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-house h-6 w-6"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
+          <title>Main</title>
+        </svg>
+      </button>
       <div className="mb-10 text-center text-indigo-400">
         <h1 className="text-3xl font-bold tracking-widest">RESET</h1>
         <p>

8bbba5a563a62897170529099d049a6c12f7bb71
LiwZhong    (¬_¬") <74370274+VanTrung0307@users.noreply.github.com>
2023-09-20 16:03:24 +0700

    leader navbar fix

--- src/components/Leaderboard/LearderboardNavbar.jsx
+++ src/components/Leaderboard/LearderboardNavbar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState, useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { menu, close } from "../../assets";
 import { AuthContext } from "../../authentication/AuthContext";
@@ -35,6 +35,8 @@ const LearderboardNavbar = () => {
   const handleDropdownToggle = () => {
     setOpen((prevState) => !prevState);
   };
+
+  const navigate = useNavigate();
 
   const { loggedIn, logout, nickname } = useContext(AuthContext);
 
@@ -105,7 +107,7 @@ const LearderboardNavbar = () => {
                       <ul className="space-y-3 dark:text-white">
                         <li className="font-medium">
                           <a
-                            href="/account"
+                            onClick={() => navigate("account")}
                             className="flex items-center transform transition-colors duration-200 border-r-4 border-transparent hover:border-orange-500"
                           >
                             <div className="mr-3">
@@ -127,36 +129,6 @@ const LearderboardNavbar = () => {
                             Account
                           </a>
                         </li>
-                        {/* <li className="font-medium">
-                          <a
-                            href="#"
-                            className="flex items-center transform transition-colors duration-200 border-r-4 border-transparent hover:border-orange-500"
-                          >
-                            <div className="mr-3">
-                              <svg
-                                className="w-6 h-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  strokeWidth="2"
-                                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                                ></path>
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  strokeWidth="2"
-                                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                ></path>
-                              </svg>
-                            </div>
-                            Setting
-                          </a>
-                        </li> */}
                         <hr className="dark:border-gray-700" />
                         <li className="font-medium">
                           <a
